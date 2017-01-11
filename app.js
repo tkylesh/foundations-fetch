8c0528452eb6c08f16054752c22dc872f3074506
Taylor Shuff <tkylesh@gmail.com>
2017-01-10 21:28:17 -0600

    getting response for 200 status.

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
-(function(){
-	var app = angular.module('myApp', []);
+;(function(){
+	const app = angular.module('fetchApp', []);
 
-	app.controller('AppController', function() {});
+	// debugger
+	// http://httpstat.us/200
+
+	app.controller('MainController', function($scope, requestService, Response) {
+		$scope.responses = [];
+		$scope.url = "http://httpstat.us/200";
+		$scope.message = 'Foundation-Fetch';
+		$scope.fetch = () => {
+			requestService.get($scope.url).then(
+				(success) =>{
+					// debugger
+					const response = new Response(success);
+					$scope.responses.push(response);
+				}, (error) =>{
+					// debugger
+					$scope.responses.push({statusCode: error.status});
+				});
+		};
+	});
+
+	app.service('requestService', function($http) {
+		this.get = (url) => $http.get(url);
+	});
+
+	app.factory('Response', function (){
+		const Response = function(responseData){
+			this.statusCode = responseData.status;
+		};
+
+		return Response;
+	});
+
 
 })();
