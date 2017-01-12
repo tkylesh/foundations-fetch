d7cbbd274a3f26bcda5a7b479e25311a50e5a92f
Taylor Shuff <tkylesh@gmail.com>
2017-01-12 09:40:03 -0600

    added response time in ms to all methods and changed default url.

--- mainController.js
+++ mainController.js
@@ -1,7 +1,7 @@
 ;(function () {
   angular.module('fetchApp').controller('MainController', function ($scope, requestService, Response) {
     $scope.responses = [];
-    $scope.url = "http://httpstat.us/200";
+    $scope.url = "https://nss-responsecodes.herokuapp.com/200";
     $scope.method = 'GET';
 
     $scope.fetch = () => {
@@ -24,30 +24,42 @@
 		      });
 		    break;
 		    case 'POST':
+		    	var sendDate = (new Date()).getTime();
 		    	requestService.post($scope.url).then((success) => {
 		        const response = new Response(success);
 		        $scope.responses.push(response);
 		        console.log('responses', $scope.responses);
+		        var receiveDate = (new Date()).getTime();
+		        var responseTimeMs = receiveDate - sendDate;
+        		console.log('response time: ', `${responseTimeMs} ms`);
 		      }, (error) => {
 		        debugger
 		        //do something else
 		      });
 		    break;
 		    case 'OPTIONS':
+		    	var sendDate = (new Date()).getTime();
 		    	requestService.options($scope.url).then((success) => {
 		        const response = new Response(success);
 		        $scope.responses.push(response);
 		        console.log('responses', $scope.responses);
+		        var receiveDate = (new Date()).getTime();
+		        var responseTimeMs = receiveDate - sendDate;
+        		console.log('response time: ', `${responseTimeMs} ms`);
 		      }, (error) => {
 		        debugger
 		        //do something else
 		      });
 		    break;
 		    case 'HEAD':
+		    	var sendDate = (new Date()).getTime();
 		    	requestService.head($scope.url).then((success) => {
 		        const response = new Response(success);
 		        $scope.responses.push(response);
 		        console.log('responses', $scope.responses);
+		        var receiveDate = (new Date()).getTime();
+		        var responseTimeMs = receiveDate - sendDate;
+        		console.log('response time: ', `${responseTimeMs} ms`);
 		      }, (error) => {
 		        debugger
 		        //do something else
