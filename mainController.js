412afc144d1eb702247ea4c310a3e0a28ef0da49
Taylor Shuff <tkylesh@gmail.com>
2017-01-13 13:35:20 -0600

    assignment complete except for response size.

--- mainController.js
+++ mainController.js
@@ -12,10 +12,16 @@
     	switch($scope.method) {
     	case 'GET':
     		var sendDate = (new Date()).getTime();
+
 		      requestService.get($scope.url).then((success) => {
 		        const response = new Response(success);
 		        $scope.responses.push(response);
+		        //get content size of the response
+		        // console.log('content size of response: ', `${Buffer.byteLength(response.data.hello, 'utf8')} bytes`)
+		        
 		        console.log('responses', $scope.responses);
+
+		        //calculate response time in ms
 		        var receiveDate = (new Date()).getTime();
         		$scope.responseTimeMs = receiveDate - sendDate;
         		console.log('response time: ', `${$scope.responseTimeMs} ms`);
